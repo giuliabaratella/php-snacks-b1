4c7cca4b684d1af2ca029ad56c5f5742e99cdb19
giuliabaratella <giuliabaratella7@gmail.com>
2023-11-28 15:33:20 +0100

    added array + printed with foreach / Snack 1

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <!-- php  -->
 <?php
+$matches = [
+    [
+        'team_home' => 'CLEVELAND CAVALIERS',
+        'team_away' => 'GOLDEN STATE WARRIORS',
+        'goals_home' => '62',
+        'goals_away' => '75'
+    ],
+    [
+        'team_home' => 'LOS ANGELES LAKERS',
+        'team_away' => 'MILWAUKEE BUCKS',
+        'goals_home' => '83',
+        'goals_away' => '68'
+    ],
+    [
+        'team_home' => 'TORONTO RAPTORS',
+        'team_away' => 'SAN ANTONIO SPURS',
+        'goals_home' => '91',
+        'goals_away' => '87'
+    ],
+];
 
 ?>
 
@@ -16,6 +36,12 @@
 
 <body>
     <h1>Snack 1</h1>
+    <h2>Partite della stagione</h2>
+    <?php foreach ($matches as $match) { ?>
+        <div>
+            <?php echo "{$match['team_home']} - {$match['team_away']} | {$match['goals_home']} - {$match['goals_away']} " ?>
+        </div>
+    <?php } ?>
 
 </body>
 
